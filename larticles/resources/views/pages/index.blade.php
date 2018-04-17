b04abc1920dc537309c6a5d97c391e9c18d8ea8d
Cole Williams <cdubs@Coles-MacBook-Pro.local>
2018-04-17 16:38:47 -0600

    updated db settings

--- larticles/resources/views/pages/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-	<div class="jumbotron text-center">
-		<h1>{{$title}}</h1>
-		<p>this is the laravel app im building</p>
-		<a href="/login" role="button" class="btn btn-primary btn-lg">Login</a>
-	</div>
-@endsection
